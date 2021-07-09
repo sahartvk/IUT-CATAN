7e10309f95dc26f5b2d4c52857f50660055e4816
Fatemeh-Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2021-07-09 11:40:16 +0430

    create dice.cpp

---
+++ dice.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include "dice.h"
+using namespace std:
+
+Dice::Dice() {
+	value = 0;
+}
+void Dice::roll() {
+	srand(time(NULL));
+	int num;
+	num = rand() % 6 + 1;
+	value = num;
+}
+int Dice::getvalue() {
+	return value;
+}
