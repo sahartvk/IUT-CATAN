72aa5123daff414d8a9d9b5e6fef2a0cdabf06be
haniehebr <80788767+haniehebr@users.noreply.github.com>
2021-07-09 15:38:07 +0430

    create Seaport.cpp

---
+++ Seaport.cpp
@@ -0,0 +1,15 @@
+#include "Seaport.h"
+Seaport::Seaport() {};
+void Seaport::setSpecial(bool b) { special = b; };
+bool Seaport::getSpecial(void) { return special; };
+bool Seaport::trade()
+{
+	if (special) 
+	{
+
+	}
+	else
+	{
+
+	}
+}
