6d565717bff79721f593d3d77b82cb75cf3df21b
Fatemeh-Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2021-07-09 11:43:14 +0430

    Create player.h

---
+++ player.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<iostream>
+#include<vector>
+
+class player {
+public:
+	Player();
+	int update();
+	void draw();
+	void getusername();
+	bool tradeWithBank();
+	bool tradeWithPlayers();
+
+private:
+	vector<resource_card>resources;
+	vector<development_card>development;
+	vector<road>roads;
+	vector<settlement>homes;
+	vector<city>cities;
+	int victorypoint;
+	std::string username;
+	Dice first;
+	Dice second;
+
+};
