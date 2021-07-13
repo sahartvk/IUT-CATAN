355a76c3be005182b8c4b410643c473f1a83c665
Fatemeh-Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2021-07-13 15:59:56 +0430

    update_resource_and_trade_need_correction

--- player.cpp
+++ player.cpp
@@ -2,26 +2,55 @@
 #include "player.h"
 #include "Tile.h"
 #include "trade.h"
+#include "RecourceCard.h"
 using namespace std;
 
 Player::Player() {
 	victorypoint = 0;
 }
-void Player::setusername(string user) {
+void Player:: setusername (string user) {
 	username = user;
 }
 
-string Player::getusername(){
+string Player:: getusername () {
 	return username;
 }
-int Player:: update() {
+
+int  Player::update_point() {
+	victorypoint = homes.size() + cities.size() * 2;
+}
+
+void  Player::update_resource(string resource) {
+	//according to neighbors:(
+}
+
+void  Player::update_building(string building) {//string like S 2 | C 5 | R 7
+	//split string and update vectors
+	stringstream ss(building);
+	string word;
+	vector<string>b;
+	while (ss >> word) {
+		b.push_back(word);
+	}
+	int number = stoi(b[1]);
+
+	if (b[0] == "C")
+		cities.push_back(number);
+
+	if (b[0] == "S")
+		homes.push_back(number);
+
+	if (b[0] == "R")
+		roads.push_back(number);
+
+}
+int Player::sum_dice_num() {
 	first.roll();
 	second.roll();
-	int n=first.getvalue() + second.getvalue();
-	// get resource from neighbor
-	victorypoint = homes.size() + cities.size() * 2;
-	return victorypoint;
+
+	return(first.getvalue() + second.getvalue());
 }
+
 
 /*bool Player::tradeWithBank() {
 	//get ratio1,2
@@ -30,14 +59,14 @@ int Player:: update() {
 	//get vector of resources
 	//vector<ResourceCar>t1, t2;
 	//t.trading(t1, t2);
-
 }*/
+
 bool Player::tradeWithPlayers() {
 	//get ratio1,2
 	int a, b;
-	trade t(a, b);
+	Trade t(a, b);
 	//get vector of resources
-	vector<ResourceCar>t1, t2;
+    vector<RecourceCard>t1, t2;
 	t.trading(t1, t2);
 
 }
