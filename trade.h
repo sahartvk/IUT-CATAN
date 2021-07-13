1a4bbe325ba88ee40be63652e01803e6b44d2bec
Fatemeh-Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2021-07-13 16:02:31 +0430

    correct_including

--- trade.h
+++ trade.h
@@ -1,12 +1,12 @@
 #pragma once
-#include "Resources.h"
+#include "RecourceCard.h"
 #include<vector>
 
 class Trade {
 public:
 	Trade(int , int);
 	void changeratio(int, int);
-	bool trading(vector<ResourceCard>, vector<ResourceCard>);
+	bool trading(vector<RecourceCard>, vector<RecourceCard>);
 private:
 	int ratio1;
 	int ratio2;
