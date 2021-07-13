f8d94f8b79c256038cd00f511c222a53f4e69d83
Fatemeh-Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2021-07-13 16:01:19 +0430

    Update player.h

--- player.h
+++ player.h
@@ -1,23 +1,29 @@
 #pragma once
 #include<iostream>
 #include<vector>
-
-class player {
+#include "dice.h"
+#include "RecourceCard.h"
+#include "DevelopmentCard.h"
+#include <sstream>
+#include <map>
+class Player {
 public:
 	Player();
-	int update();
-	void draw();
+	int update_point();
+	void update_resource(string);
+	void update_building(string);
+	int sum_dice_num();
 	void setusername(string);
-        string getusername();
+	string getusername();
 	bool tradeWithBank();
 	bool tradeWithPlayers();
 
 private:
-	vector<resource_card>resources;
-	vector<development_card>development;
-	vector<road>roads;
-	vector<settlement>homes;
-	vector<city>cities;
+	vector<RecourceCard>resources;
+	vector<DevelopmentCard>development;
+	vector<int>roads;
+	vector<int>homes;
+	vector<int>cities;
 	int victorypoint;
 	std::string username;
 	Dice first;
