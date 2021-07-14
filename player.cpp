8c2013a35b004da499048051f1a70e465dc8adea
Fatemeh-Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2021-07-14 20:12:15 +0430

    edit player class

--- player.cpp
+++ player.cpp
@@ -1,30 +1,61 @@
 #include<iostream>
 #include "player.h"
-#include "Tile.h"
-#include "trade.h"
-#include "RecourceCard.h"
+
 using namespace std;
 
-Player::Player() {
+Player::Player(string _color) {
+	color = _color;
 	victorypoint = 0;
 }
-void Player:: setusername (string user) {
+void Player::setusername (string user) {
 	username = user;
 }
 
-string Player:: getusername () {
+string Player::getusername () {
 	return username;
 }
 
-int  Player::update_point() {
-	victorypoint = homes.size() + cities.size() * 2;
+void Player::update_victorypoint() {
+	victorypoint++;
+}
+void Player::update_Developmentcard(string card) {
+	//push_back DEV card in vector
+	DevelopmentCard dev("development_card",card);
+	development.push_back(dev);
 }
 
-void  Player::update_resource(string resource) {
-	//according to neighbors:(
+void Player::update_resource(string resource) {
+	//split string and update resource  
+    //example: 3-wood
+
+	vector<string>u;
+	string T;
+	stringstream X(resource);
+	while (getline(X, T, '-')) {
+		u.push_back(T);
+	}
+	string _count = u[0];
+	string resource = u[1];
+
+	int count = stoi(_count);
+	if (resource == "wood")
+		wood_cnt += count;
+
+	if (resource == "sheap")
+		sheap_cnt += count;
+
+	if (resource == "stone")
+		stone_cnt += count;
+
+	if (resource == "brick")
+		brick_cnt += count;
+
+	if (resource == "wheat")
+		wheat_cnt += count;
+
 }
 
-void  Player::update_building(string building) {//string like S 2 | C 5 | R 7
+void Player:: update_building (string building) {//string like S 2 | C 5 | R 7
 	//split string and update vectors
 	stringstream ss(building);
 	string word;
@@ -32,41 +63,68 @@ void  Player::update_building(string building) {//string like S 2 | C 5 | R 7
 	while (ss >> word) {
 		b.push_back(word);
 	}
-	int number = stoi(b[1]);
+	
 
-	if (b[0] == "C")
-		cities.push_back(number);
+	if (b[0] == "C") {
+		Building city("city");
+		buildings.push_back(city);
+	}
+	if (b[0] == "S") {
+		Building settelment("home");
+		buildings.push_back(settelment);
+	}
 
-	if (b[0] == "S")
-		homes.push_back(number);
-
-	if (b[0] == "R")
-		roads.push_back(number);
+	if (b[0] == "R") {
+		Building road("road");
+		buildings.push_back(road);
+	}
 
 }
-int Player::sum_dice_num() {
+int Player :: roll_dice () {
+	Dice first;
+	Dice second;
 	first.roll();
 	second.roll();
 
 	return(first.getvalue() + second.getvalue());
 }
 
+bool Player:: tradeWithBank (string give, string receive) {
 
-/*bool Player::tradeWithBank() {
-	//get ratio1,2
-	int a, b;
-	trade t(a, b);
-	//get vector of resources
-	//vector<ResourceCar>t1, t2;
-	//t.trading(t1, t2);
-}*/
+	Trade t(4, 1);
+	t.trading_bank(give, receive);
+}
 
-bool Player::tradeWithPlayers() {
-	//get ratio1,2
+bool Player::tradeWithPlayers() {  //not complte
+	
 	int a, b;
 	Trade t(a, b);
 	//get vector of resources
     vector<RecourceCard>t1, t2;
 	t.trading(t1, t2);
 
+}
+
+vector<string> Player:: can_Buy_Building() {
+	vector<string>can_buy;
+	//check road
+	if (wood_cnt >= 1 && brick_cnt >= 1)
+		can_buy.push_back("road");
+        //check settlement
+	if (wood_cnt >= 1 && brick_cnt >= 1 && sheap_cnt >= 1 && wheat_cnt >= 1)
+		can_buy.push_back("settlement");
+        //check city
+	if (stone_cnt >= 3 && wheat_cnt >= 2)
+		can_buy.push_back("city");
+
+	return can_buy;
+
+}
+bool Player::can_buy_Dev_card() {
+	if (stone_cnt >= 1 && sheap_cnt >= 1 && wheat_cnt >= 1)
+		return true;
+	return false;
+}
+int Player::get_victorypoint() {
+	return victorypoint;
 }
