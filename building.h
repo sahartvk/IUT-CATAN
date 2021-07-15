a604f8e5f202fe918a0efd33fc5f7810ceee3c02
haniehebr <80788767+haniehebr@users.noreply.github.com>
2021-07-15 12:32:53 +0430

    edit building.h

--- building.h
+++ building.h
@@ -1,21 +1,14 @@
 #pragma once
-#include"Location.h"
-#include"Tile.h"
-
-enum class BuildingType {
-	ROAD,
-	SETTLEMENT,
-	CITY,
-	BRIDGE,
-};
 class Building {
 private:
-	int number;
-	BuildingType type;
+	int location;
+	string type;
 	string color;
 	int point;
 public:
-	Building(int _number, BuildingType _type, string _color);
-	int getPoint();
+	Building(int _location, string _type, string _color);
+	string getType();
 	string getColor();
+	int getLocation();
+	int getPoint();
 };
