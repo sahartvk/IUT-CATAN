9ed85ca281e159418d39047de17d21c33038f3a5
haniehebr <80788767+haniehebr@users.noreply.github.com>
2021-07-15 12:34:50 +0430

    edit constructor of seaport.cpp

--- Seaport.cpp
+++ Seaport.cpp
@@ -1,15 +1,10 @@
 #include "Seaport.h"
-Seaport::Seaport() {};
-void Seaport::setSpecial(bool b) { special = b; };
-bool Seaport::getSpecial(void) { return special; };
-bool Seaport::trade()
+Seaport::Seaport(string resource_, int ratio1_, int ratio2_)
 {
-	if (special) 
-	{
-
-	}
-	else
-	{
-
-	}
+	resource = resource_;
+	ratio1 = ratio1_;
+	ratio2 = ratio2_;
 }
+int Seaport::getRatio1() { return ratio1; }
+int Seaport::getRatio2() { return ratio2; }
+string Seaport::getResource() { return resource; }
