fbb5829262e72d265094371a6f6ec9ded634181d
Fatemeh-Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2021-07-16 21:34:10 +0430

    edit trade (seaport not complete)

--- trade.cpp
+++ trade.cpp
@@ -1,15 +1,58 @@
 #include<iostream>
 #include "trade.h"
+#include "RecourceCard.h"
 using namespace std;
-Trade::Trade(int _ratio1, int _ratio2) {
-	ratio1 = _ratio1;
-	ratio2 = _ratio2;
+Trade::Trade(vector<RecourceCard>& resource, vector<Seaport>& seaports) {
+	sources = resource;
+	ports = seaports;
 }
-void Trade::changeratio(int _ratio1, int _ratio2) {
-	ratio1 = _ratio1;
-	ratio2 = _ratio2;
+bool Trade::trade_bank(string give,string recieve) {
+	ratio1 = 4;
+	ratio2 = 1;
+	int count = 0;
+	for (int i = 0; i < sources.size(); i++) {
+		if (sources[i].gettype() == give)
+			count++;
+	}
+	if (count >= 4) {
+		for (int i = 0; count != 0; i++, count--) {
+			if (sources[i].gettype() == give)
+				sources.erase(sources.begin() + i);
+		}
+		RecourceCard card(recieve);
+		sources.push_back(card);
+		return true;
+	}
+	else
+		return false;
+
+
 }
-bool Trade::trading(vector<ResourceCard> give, vector<ResourceCard> recieve) {
-   //bank server;
-   //add or minase :\
+bool Trade::trade_Clients(vector<RecourceCard>_give , vector<RecourceCard>_recieve) {
+	int cnt = 0;
+	for (int i = 0; i < sources.size(); i++) {
+		for (int j = 0; j < _give.size(); j++) {
+			if (sources[i].gettype() == _give[j].gettype())
+				cnt++;
+		}
+	}
+	if (cnt >= _give.size())
+	{
+		while (cnt > 0) {
+			for (int i = 0; i < sources.size(); i++) {
+				for (int j = 0; j < _give.size(); j++) {
+					if (sources[i].gettype() == _give[j].gettype()) {
+						sources.erase(sources.begin() + i);
+						cnt--;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	else
+		return false;
+}
+bool Trade::trade_seaport() {
+	//not complete
 }
